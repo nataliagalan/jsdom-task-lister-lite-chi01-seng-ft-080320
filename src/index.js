884988fb77edd42c7115b93f5723fedda7a2557d
Natalia <n8galan@gmail.com>
2020-09-15 16:37:36 -0500

    finished core deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+ 
+  
+    let submitBtn = document.getElementById('submit-btn')
+    let form = document.querySelector('form')
+
+    form.addEventListener('submit', function(event){
+      event.preventDefault()
+      let task = document.querySelector('#new-task-description').value 
+      let newTask = document.createElement('li')
+      newTask.innerHTML = task
+      
+      document.querySelector('#tasks').appendChild(newTask)
+
+    })
+
+
+
+
 });
